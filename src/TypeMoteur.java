57ddf0220ba1740abded7140391568660c058fd5
RicoBSJ <aubrun.eric@free.fr>
2019-02-10 11:37:12 +0100

    2 Commit Holidays 10_02

---
+++ src/TypeMoteur.java
@@ -0,0 +1,14 @@
+
+public enum TypeMoteur {
+	
+	String moteurDiesel = "DIESEL";
+	String moteurElectrique = "ELECTRIQUE";
+	String moteurEssence = "ESSENCE";
+	String moteurHybride = "HYBRIDE";
+	
+	private TypeMoteur(String string, double d) {
+
+		// TODO Auto-generated constructor stub
+		return;
+	}
+}
